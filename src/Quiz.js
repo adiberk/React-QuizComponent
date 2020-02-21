ed4fd6e423fa427cdc1df14bffd2f40bb04e99a4
Adi Berkowitz <berkowitzadi@gmail.com>
2020-02-21 14:38:58 -0500

    part-5 done

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,10 +3,16 @@ import QuizQuestion from './QuizQuestion'
 import QuizEnd from './QuizEnd'
 let quizData=require('./quiz_data.json')
 
+
 class Quiz extends Component{
     constructor(props){
         super(props);
         this.state={quiz_position: 1}
+    }
+    showNextQuestion() {
+        this.setState((state) => {
+          return {quiz_position: state.quiz_position + 1}
+        })
     }
     render() {
         const isQuizEnd= (this.state.quiz_position-1)=== quizData.quiz_questions.length
@@ -14,7 +20,7 @@ class Quiz extends Component{
             <div>
                 {isQuizEnd ?
                     <QuizEnd /> :
-                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
+                    <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
                 }
             </div>
         )
